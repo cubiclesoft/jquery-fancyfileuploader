af8174b92b906b0126ea7754b247e96913eb97b4
cubiclesoft <webmaster@cubiclesoft.com>
2022-03-23 17:44:20 -0700

    Fixed spacing issues.

--- fancy-file-uploader/lang/fr.js
+++ fancy-file-uploader/lang/fr.js
@@ -1,86 +1,86 @@
 // French translation provided courtesy of GitHub user tuxfamily.
 // (C) 2022 CubicleSoft.  All Rights Reserved.
 
-if (!$.fn.FancyFileUpload.langs) $.fn.FancyFileUpload.langs = {};
+if (!$.fn.FancyFileUpload.langs)  $.fn.FancyFileUpload.langs = {};
 
 $.fn.FancyFileUpload.langs['fr'] = {
-    'Start uploading':
-        'Démarrer le transfert',
+	'Start uploading':
+		'Démarrer le transfert',
 
-    'Starting upload...':
-        'Démarrage du transfert...',
+	'Starting upload...':
+		'Démarrage du transfert...',
 
-    'Upload completed':
-        'Transfert terminé',
+	'Upload completed':
+		'Transfert terminé',
 
-    'There is a file upload still in progress.  Leaving the page will cancel the upload.\n\nAre you sure you want to leave this page?':
-        'Un transfert de fichier est toujours en cours. Si vous quittez la page, il sera annulé.\n\nVoulez-vous vraiment quitter cette page ?',
+	'There is a file upload still in progress.  Leaving the page will cancel the upload.\n\nAre you sure you want to leave this page?':
+		'Un transfert de fichier est toujours en cours. Si vous quittez la page, il sera annulé.\n\nVoulez-vous vraiment quitter cette page ?',
 
-    'There is a file that was added to the queue but the upload has not been started.  Leaving the page will clear the queue and not upload the file.\n\nAre you sure you want to leave this page?':
-        'Un fichier a été ajouté à la file d\'attente, mais le transfert n\'a pas démarré. Quitter la page effacera la file d\'attente et ne téléversera pas le fichier.\n\nVoulez- vous vraiment quitter cette page ?',
+	'There is a file that was added to the queue but the upload has not been started.  Leaving the page will clear the queue and not upload the file.\n\nAre you sure you want to leave this page?':
+		'Un fichier a été ajouté à la file d\'attente, mais le transfert n\'a pas démarré. Quitter la page effacera la file d\'attente et ne téléversera pas le fichier.\n\nVoulez- vous vraiment quitter cette page ?',
 
-    'Cancel upload and remove from list':
-        'Annuler le transfert et le supprimer de la liste',
+	'Cancel upload and remove from list':
+		'Annuler le transfert et le supprimer de la liste',
 
-    'This file is currently being uploaded.\n\nStop the upload and remove the file from the list?':
-        'Ce fichier est en cours de transfert.\n\nArrêter le transfert et supprimer le fichier de la liste ?',
+	'This file is currently being uploaded.\n\nStop the upload and remove the file from the list?':
+		'Ce fichier est en cours de transfert.\n\nArrêter le transfert et supprimer le fichier de la liste ?',
 
-    'This file is waiting to start.\n\nCancel the operation and remove the file from the list?':
-        'Ce fichier est en attente de transfert.\n\nAnnuler l\'opération et supprimer le fichier de la liste ?',
+	'This file is waiting to start.\n\nCancel the operation and remove the file from the list?':
+		'Ce fichier est en attente de transfert.\n\nAnnuler l\'opération et supprimer le fichier de la liste ?',
 
-    'Preview':
-        'Aperçu',
+	'Preview':
+		'Aperçu',
 
-    'No preview available':
-        'Aucun aperçu disponible',
+	'No preview available':
+		'Aucun aperçu disponible',
 
-    'Invalid file extension.':
-        'Extension de fichier invalide.',
+	'Invalid file extension.':
+		'Extension de fichier invalide.',
 
-    'File is too large.  Maximum file size is {0}.':
-        'Fichier trop volumineux. Le poids maximum acceptée est de {0}.',
+	'File is too large.  Maximum file size is {0}.':
+		'Fichier trop volumineux. Le poids maximum acceptée est de {0}.',
 
-    'Remove from list':
-        'Retirer de la liste',
+	'Remove from list':
+		'Retirer de la liste',
 
-    '{0} of {1} | {2}%':
-        '{0} sur {1} | {2}%',
+	'{0} of {1} | {2}%':
+		'{0} sur {1} | {2}%',
 
-    '{0} | Network error, retrying in a moment... ({1})':
-        '{0} | Erreur réseau, nouvelle tentative dans un instant... ({1})',
+	'{0} | Network error, retrying in a moment... ({1})':
+		'{0} | Erreur réseau, nouvelle tentative dans un instant... ({1})',
 
-    'The upload was cancelled.':
-        'Le transfert a été annulé.',
+	'The upload was cancelled.':
+		'Le transfert a été annulé.',
 
-    'The upload failed.  {0} ({1})':
-        'Le transfert a échoué.  {0} ({1})',
+	'The upload failed.  {0} ({1})':
+		'Le transfert a échoué.  {0} ({1})',
 
-    'The upload failed.':
-        'Le transfert a échoué.',
+	'The upload failed.':
+		'Le transfert a échoué.',
 
-    'The server indicated that the upload was not successful.  No additional information available.':
-        'Le serveur a indiqué que le transfert a échoué sans donner d\'informations supplémentaires.',
+	'The server indicated that the upload was not successful.  No additional information available.':
+		'Le serveur a indiqué que le transfert a échoué sans donner d\'informations supplémentaires.',
 
-    'Browse, drag-and-drop, or paste files to upload':
-        'Parcourir, glisser-déposer ou coller des fichiers à transférer',
+	'Browse, drag-and-drop, or paste files to upload':
+		'Parcourir, glisser-déposer ou coller des fichiers à transférer',
 
-    'Record audio using a microphone':
-        'Enregistrer de l\'audio à l\'aide d\'un microphone',
+	'Record audio using a microphone':
+		'Enregistrer de l\'audio à l\'aide d\'un microphone',
 
-    'Audio recording - {0}.mp3':
-        'Enregistrement audio - {0}.mp3',
+	'Audio recording - {0}.mp3':
+		'Enregistrement audio - {0}.mp3',
 
-    'Unable to record audio.  Either a microphone was not found or access was denied.':
-        'Impossible d\'enregistrer le son. Soit le microphone n\'a pas été trouvé, soit son accès a été refusé.',
+	'Unable to record audio.  Either a microphone was not found or access was denied.':
+		'Impossible d\'enregistrer le son. Soit le microphone n\'a pas été trouvé, soit son accès a été refusé.',
 
-    'Record video using a camera':
-        'Enregistrer une vidéo à l\'aide d\'une caméra',
+	'Record video using a camera':
+		'Enregistrer une vidéo à l\'aide d\'une caméra',
 
-    'Video recording - {0}.mp4':
-        'Enregistrement vidéo - {0}.mp4',
+	'Video recording - {0}.mp4':
+		'Enregistrement vidéo - {0}.mp4',
 
-    'Unable to record video.  Either a camera was not found or access was denied.':
-        'Impossible d\'enregistrer la vidéo. Soit la caméra n\'a pas été trouvée, soit son accès a été refusé.'
+	'Unable to record video.  Either a camera was not found or access was denied.':
+		'Impossible d\'enregistrer la vidéo. Soit la caméra n\'a pas été trouvée, soit son accès a été refusé.'
 };
 
 $.fn.FancyFileUpload.defaults.langmap = $.fn.FancyFileUpload.langs['fr'];
--- server-side-helpers/lang/fr.php
+++ server-side-helpers/lang/fr.php
@@ -6,48 +6,48 @@
 
 	$langmap["fr"] = array(
 		"The uploaded file exceeds the 'upload_max_filesize' directive in 'php.ini'." =>
-            "Le poids du fichier dépasse la directive 'upload_max_filesize' dans 'php.ini'.",
+			"Le poids du fichier dépasse la directive 'upload_max_filesize' dans 'php.ini'.",
 
 		"The uploaded file exceeds the 'MAX_FILE_SIZE' directive that was specified in the submitted form." =>
-            "Le poids du fichier dépasse la directive 'MAX_FILE_SIZE' spécifiée dans le formulaire.",
+			"Le poids du fichier dépasse la directive 'MAX_FILE_SIZE' spécifiée dans le formulaire.",
 
 		"The uploaded file was only partially uploaded." =>
-            "Le fichier n'a été que partiellement transféré.",
+			"Le fichier n'a été que partiellement transféré.",
 
 		"No file was uploaded." =>
 			"Aucun fichier n'a été transféré.",
 
 		"The configured temporary folder on the server is missing." =>
-            "Le dossier temporaire configuré sur le serveur n'existe pas.",
+			"Le dossier temporaire configuré sur le serveur n'existe pas.",
 
 		"Unable to write the temporary file to disk.  The server is out of disk space, incorrectly configured, or experiencing hardware issues." =>
-            "Impossible d'écrire le fichier temporaire sur le disque. Le serveur manque d'espace disque ou rencontre des problèmes techniques.",
+			"Impossible d'écrire le fichier temporaire sur le disque. Le serveur manque d'espace disque ou rencontre des problèmes techniques.",
 
 		"A PHP extension stopped the upload." =>
-            "Une extension PHP a arrêté le transfert.",
+			"Une extension PHP a arrêté le transfert.",
 
 		"An unknown error occurred." =>
-            "Une erreur inconnue est survenue.",
+			"Une erreur inconnue est survenue.",
 
 		"The specified input filename was not uploaded to this server." =>
-            "Le nom de fichier spécifié n'a pu être transmis au serveur.",
+			"Le nom de fichier spécifié n'a pu être transmis au serveur.",
 
 		"File data was submitted but is missing." =>
-            "Les données du fichier ont été transférées mais le fichier est manquant.",
+			"Les données du fichier ont été transférées mais le fichier est manquant.",
 
 		"Invalid file extension.  Must be one of %s." =>
-            "Extension de fichier invalide. Les extensions acceptées sont : %s.",
+			"Extension de fichier invalide. Les extensions acceptées sont : %s.",
 
 		"The server did not set a valid filename." =>
-            "Le serveur n'a pas retourné un nom de fichier valide.",
+			"Le serveur n'a pas retourné un nom de fichier valide.",
 
 		"The server file size limit was exceeded." =>
-            "La limite de poids de fichier définie sur le serveur a été dépassée.",
+			"La limite de poids de fichier définie sur le serveur a été dépassée.",
 
 		"Unable to open a required file for writing." =>
-            "Impossible d'ouvrir un fichier requis pour l'écriture.",
+			"Impossible d'ouvrir un fichier requis pour l'écriture.",
 
 		"Unable to open a required file for reading." =>
-            "Impossible d'ouvrir un fichier requis pour la lecture."
+			"Impossible d'ouvrir un fichier requis pour la lecture."
 	);
 ?>
